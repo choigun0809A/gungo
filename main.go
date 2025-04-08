5dd80ed3cc3e2d9549cd9a4316a9ecae89efe6c2
choigun0809A <122153202+choigun0809A@users.noreply.github.com>
2025-04-08 15:52:18 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,44 @@
+package gungo
+
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+type Game struct {
+	Screen *ebiten.Image
+}
+
+func (G *Game) Draw(screen *ebiten.Image) {
+	G.Screen = screen
+}
+func (G *Game) Update() error {
+	return nil
+}
+func (G *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return outsideWidth, outsideHeight
+}
+
+var (
+	GG *Game
+)
+
+func Setscreen(width int, height int, name string) {
+	GG = &Game{}
+	ebiten.SetWindowSize(width, height)
+	ebiten.SetWindowTitle(name)
+	go func() {
+		ebiten.RunGame(GG)
+	}()
+
+}
+
+func DrawRectBorder(x float32, y float32, width float32, height float32, color color.Color, border_width float32, antialias bool) {
+	vector.StrokeRect(GG.Screen, x, y, width, height, border_width, color, antialias)
+}
+
+func DrawRectFilled(x float32, y float32, width float32, height float32, color color.Color, antialias bool) {
+	vector.DrawFilledRect(GG.Screen, x, y, width, height, color, antialias)
+}
